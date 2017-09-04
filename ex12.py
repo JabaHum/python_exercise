353fa9e29830ff6d9c248497ab4cf65c0a40d683
JabaHum <jabajnan8@gmail.com>
2017-09-05 00:41:41 +0300

    Adding ex12.py

---
+++ ex12.py
@@ -0,0 +1,25 @@
+'''Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+Suppose the following input is supplied to the program:
+without,hello,bag,world
+Then, the output should be:
+bag,hello,without,world
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
+Solution:
+items=[x for x in raw_input().split(',')]
+items.sort()
+print ','.join(items)
+#----------------------------------------#
+'''
+ 
+ #!/usr/bin/env python
+ #-*- coding:utf-8 -*-
+ 
+ 
+import sys
+
+items=[x for x in raw_input().split(',')]
+items.sort()
+print ','.join(items)
